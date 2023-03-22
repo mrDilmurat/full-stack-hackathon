6cd9a874a439138c613ac62ce3cd508f863bec6b
Alisher Kasuev <alisherkasuev@MacBook-Pro-Alisher.local>
2023-03-22 16:14:12 +0600

    Ad cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,9 +3,37 @@ import Container from '../Container/Container';
 import './style.css'
 
 const Cart = () => {
+
+  const cartStorage = JSON.parse(localStorage.getItem("cart"))
+
+  console.log(cartStorage);
+
+  //custom
+  const deleteCart = (id) => {
+    const deletedObj = cartStorage.products.find(item => item.id == id)
+    cartStorage.totalPrice -= deletedObj.price
+    const cartUpdated = cartStorage.products.filter((elem) => elem.id !== id);
+    cartStorage.products = cartUpdated
+    cartStorage.ticketsCount --;
+    localStorage.setItem("cart", JSON.stringify(cartStorage))
+    window.location.reload()
+  }
+
   return (
     <Container>
-      <h2 className='title'>Ваша корзина пуста</h2>
+      <h1 className='cart-cor'>КОРЗИНА</h1><hr/>
+      {cartStorage.products?.map(item => (
+        <div key={item.id}>
+          <h1 className='cart-name'>{item.name}</h1>
+          <h4>Стоимость: {item.price} kg;</h4>
+          <img className='cart-img' src={item.image} alt="" /><br /><br />
+          <button className='cart-btn' onClick={() => deleteCart(item.id)}>delete</button>
+        </div>
+      ))}
+      <hr />
+      <h4 className='cart-cor'>KОЛ: {cartStorage.ticketsCount}</h4> <hr />
+      <h4 className='cart-cor'>ОБШАЯ СУМА: {cartStorage.totalPrice}</h4> <hr />
+
     </Container>
   )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -9,11 +9,10 @@ import { BsFillCCircleFill } from "react-icons/bs";
 import Container from '../Container/Container';
 
 
-import { useState, useEffect } from 'react';
 
 
 const Products = () => {
- 
+
   const [searchParams, setSearchParams] = useSearchParams();
 
   const postQuery = searchParams.get('product') || '';
@@ -27,12 +26,11 @@ const Products = () => {
     setSearchParams({ product: query })
   }
 
-
   return (
     <>
       <Container>
         <h4 className="title">ВСЕ МЕРОПРИЯТИЯ</h4>
-        
+
         <form className='search-form' autoComplete='off' onSubmit={handleSubmit}>
           <input className='search-products' type='search' name='search' placeholder='Search' />
           <input className='btn-send' type='submit' name='Search' />
@@ -46,12 +44,14 @@ const Products = () => {
                     <Card.Img variant="top" className='product-img' src={`${product.image}`} alt="image" />
 
                     <Link className='products-link' to={`/products/${product.id}`}>КУПИТЬ БИЛЕТ</Link>
+                    {/* <button className='product-dob'>ДОБАВИТ В КОРЗИНУ</button> */}
 
                     <h6 className='products-title'>{product.name}</h6>
                     <Card.Body className=''>
                       <p className='products-text'><GoCalendar className="icons-react" />{`${product.text}`}</p>
-                      <p className='products-text'><BsFillCCircleFill className="icons-react" />{`${product.price}`}</p>
+                      <p className='products-text'><BsFillCCircleFill className="icons-react" />Стоимость: {`${product.price}`} сом</p>
                       <p className='products-text'><FaMapMarkerAlt className="icons-react" />{`${product.location}`}</p>
+
                     </Card.Body>
                     <Card.Img className='red-ok' src="/img/red-ok.png" alt="" />
                   </Card>
